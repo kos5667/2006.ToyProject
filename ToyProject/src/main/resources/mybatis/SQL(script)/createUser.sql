6c35b41ade5413fe989baa38991f35c9096a05ad
yongho1219 <12cks@naver.com>
2020-06-06 18:37:34 +0900

    테이블 생성 sql문 작성

--- ToyProject/src/main/resources/mybatis/SQL(script)/createUser.sql
+++ ToyProject/src/main/resources/mybatis/SQL(script)/createUser.sql
@@ -1,11 +1,10 @@
 /*[sqlpus 프롬프트에서 아래와 같이 작성하여 설정을 변경해 준다.]*/
 ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
 
-/* CREATE USER */
-CREATE USER TP IDENTIFIED BY 1234;
-
-/* 권한부여 */
-GRANT CREATE SESSION, CONNECT RESOURCE, DBA TO TP;
-
-/* 해당 테이블 스페이스의 용량 제한없이 사용 가능 */
---ALTER USER TP DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS;
+-- root로 로그인한 후 데이터베이스 생성
+create database TP default CHARACTER SET UTF8;
+show databases;
+use mysql;
+create user TP@localhost identified by '1234';
+GRANT ALL PRIVILEGES ON TP.* TO TP@LOCALHOST;
+------------------------------------------------
