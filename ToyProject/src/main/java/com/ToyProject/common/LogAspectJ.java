07160bb001188d96de53f0cf004415a447284578
kos5667 <kos5667@gmail.com>
2020-06-13 01:07:08 +0900

    logback

--- ToyProject/src/main/java/com/ToyProject/common/LogAspectJ.java
+++ ToyProject/src/main/java/com/ToyProject/common/LogAspectJ.java
@@ -20,25 +20,25 @@ public class LogAspectJ {
 	
 	///Constructor
 	public LogAspectJ() {
-		LOGGER.debug("\nCommon :: "+this.getClass()+"\n");
+		LOGGER.info("\nCommon :: "+this.getClass()+"\n");
 	}
 	
 	//Around  Advice
 	@Around("execution(* com.ToyProject..service.impl.*Impl.*(..) )")
 	public Object invoke(ProceedingJoinPoint joinPoint) throws Throwable {
 			
-		LOGGER.debug("");
-		LOGGER.debug("[Around before] 타겍객체.메서드 :"+
+		LOGGER.info("");
+		LOGGER.info("[Around before] 타겍객체.메서드 :"+
 													joinPoint.getTarget().getClass().getName() +"."+
 													joinPoint.getSignature().getName());
 		if(joinPoint.getArgs().length !=0){
-			LOGGER.debug("[Around before]method에 전달되는 인자 : "+ joinPoint.getArgs()[0]);
+			LOGGER.info("[Around before]method에 전달되는 인자 : "+ joinPoint.getArgs()[0]);
 		}
 		//==> 타겟 객체의 Method 를 호출 하는 부분 
 		Object obj = joinPoint.proceed();
 
-		LOGGER.debug("[Around after] 타겟 객체return value  : "+obj);
-		LOGGER.debug("");
+		LOGGER.info("[Around after] 타겟 객체return value  : "+obj);
+		LOGGER.info("");
 		
 		return obj;
 	}
