5014c5fe4cb9d9ee8040a36a25ed546f5302fabb
kos5667 <kos5667@gmail.com>
2020-06-13 17:53:53 +0900

    thymeleaf layout create

--- ToyProject/src/main/java/com/ToyProject/user/web/UserController.java
+++ ToyProject/src/main/java/com/ToyProject/user/web/UserController.java
@@ -1,10 +1,11 @@
 package com.ToyProject.user.web;
-
 import javax.annotation.Resource;
-
 import org.springframework.stereotype.Controller;
-
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import com.ToyProject.user.service.UserService;
+
 
 @Controller
 public class UserController {
@@ -12,4 +13,39 @@ public class UserController {
 	@Resource(name = "UserService")
 	UserService userService;
 	
+	@RequestMapping(value="/user/login", method=RequestMethod.GET)
+	public String getLoginform(Model model){
+		System.out.println("Controller 로그인 동작");
+		
+		return "thymeleaf/user/login";
+	}
+	@RequestMapping(value="/user/form",method = RequestMethod.GET)
+	public String getinsertUserform(Model model){
+		System.out.println("Controller 유저 생성 동작");
+		
+		return "thymeleaf/user/userCreate";
+	}
+
+	//@RequestMapping(value="/user/createuser")
+	//public void insertUser(Model model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- ToyProject/src/main/resources/static/js/common.js
+++ ToyProject/src/main/resources/static/js/common.js
@@ -1,3 +1,13 @@
 /**
- * 
+ * header.html
+ * footer.html
+ */
+
+/*  function loadScript(url, callback){
+     var script = document.createElement('script');
+     script.src = url;
+     script.onload = callback;
+
+     document.getElementsByTagName('head')[0].appendChild(script);
+ }
  */
