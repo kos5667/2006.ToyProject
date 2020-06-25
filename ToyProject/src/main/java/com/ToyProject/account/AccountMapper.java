04fdb31c702f88b47ea554225672787a67f0192c
kkp <themapisto@naver.com>
2020-06-25 09:58:38 +0900

    06-25 commit

---
+++ ToyProject/src/main/java/com/ToyProject/account/AccountMapper.java
@@ -0,0 +1,7 @@
+package com.ToyProject.account;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AccountMapper {
+    
+}
--- ToyProject/src/main/java/com/ToyProject/account/AccountRepository.java
+++ ToyProject/src/main/java/com/ToyProject/account/AccountRepository.java
@@ -4,26 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import com.ToyProject.user.vo.TestVo;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AccountRepository {
 
-    private Map<String, Account> accounts= new HashMap<>();
-    private Random random= new Random();
+    @Autowired 
+    SqlSession sqlSession;
 
 
-    public Account save(Account account){
+    private Map<String, Account> accounts = new HashMap<>();
+    private Random random = new Random();
 
+    public Account save(Account account) {
 
         account.setId(random.nextInt());
-        accounts.put(account.getEmail(),account);
+        accounts.put(account.getEmail(), account);
         return account;
 
     }
 
-
-	public Account findByEmail(String username) {
+    public Account findByEmail(String username) {
         return accounts.get(username);
-	}
+    }
+
+    public TestVo get(String email) {
+        return sqlSession.selectOne("user.getByEmail", email);
+    }
+
 }
